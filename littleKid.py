9520f2d21ddf024d007b34cfa07b8f97682487d5
dan-gheorghita <79468719+dan-gheorghita@users.noreply.github.com>
2026-01-07 14:56:39 +0200

    Add Python script

---
+++ littleKid.py
@@ -0,0 +1,6 @@
+if name == 'Alice':  # Check if the name is Alice
+    print('Hi, Alice.')  # Print a personalized greeting if name is Alice
+elif age < 12:  # Check if the age is less than 12
+    print('You are not Alice, kiddo.')  # Print a message if the person is a kid but not Alice
+else:
+    print('You are neither Alice nor a little kid.')  # Print a default message for any other case
